22fc29d6a926e0e89348e61f0a2b824c160c8f1e
yansaa267-spec <yansaa267@gmail.com>
2026-06-03 22:35:49 +0800

    laporan 4

--- routes/api.php
+++ routes/api.php
@@ -5,22 +5,29 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CategoryController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// API Version 1
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-    Route::apiResource('categories', CategoryController::class)
-        ->except(['destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
 
-    Route::delete('categories/{category}',
-        [CategoryController::class, 'destroy'])
-        ->middleware('role:admin');
+        Route::apiResource('categories', CategoryController::class)
+            ->except(['destroy']);
 
-    Route::apiResource('items', ItemController::class)
-        ->except(['destroy']);
+        Route::delete(
+            'categories/{category}',
+            [CategoryController::class, 'destroy']
+        )->middleware('role:admin');
 
-    Route::delete('items/{item}',
-        [ItemController::class, 'destroy'])
-        ->middleware('role:admin');
+        Route::apiResource('items', ItemController::class)
+            ->except(['destroy']);
+
+        Route::delete(
+            'items/{item}',
+            [ItemController::class, 'destroy']
+        )->middleware('role:admin');
+    });
+
 });
